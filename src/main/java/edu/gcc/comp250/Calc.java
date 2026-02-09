816e2acb8e309b01f85437589d55ac1c9ae5e580
MicP <miciscool.funee@gmail.com>
2026-02-09 13:35:32 -0500

    Collab changing

--- src/main/java/edu/gcc/comp250/Calc.java
+++ src/main/java/edu/gcc/comp250/Calc.java
@@ -4,4 +4,8 @@ public class Calc {
     public int add(int a, int b){
         return a+b;
     }
+
+    public int subtract(int a, int b){
+        return a-b;
+    }
 }
--- src/main/java/edu/gcc/comp250/Driver.java
+++ src/main/java/edu/gcc/comp250/Driver.java
@@ -2,11 +2,12 @@ package edu.gcc.comp250;
 
 public class Driver {
     public static void main(String[] args) {
-        System.out.println("hello mayo jar");
+        System.out.println("hello jar");
 
         Calc c = new Calc();
         System.out.println(c.add(6,7));
         System.out.println("I am so confused :(");
         System.out.println("Pls help ME");
+
     }
 }
