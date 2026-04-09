86f79597966d2fb2d18ee28900797e90265fc348
MicP <miciscool.funee@gmail.com>
2026-04-09 13:20:13 -0400

    Complaint with lint.3?

--- src/main/java/edu/gcc/comp250/Calc.java
+++ src/main/java/edu/gcc/comp250/Calc.java
@@ -1,10 +1,20 @@
 package edu.gcc.comp250;
 
 public class Calc {
+    /**
+     * @param a
+     * @param b
+     * @return
+     */
     public int add (int a, int b) {
         return a + b;
     }
 
+    /**
+     * @param a
+     * @param b
+     * @return
+     */
     public int divide (int a, int b) {
         return a / b;
     }
