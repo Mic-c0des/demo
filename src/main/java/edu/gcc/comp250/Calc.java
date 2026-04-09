31f8863937d9a07cf7568a2acb6d8efbb9b4c911
MicP <miciscool.funee@gmail.com>
2026-04-09 11:33:19 -0400

    Does lint lint?

--- src/main/java/edu/gcc/comp250/Calc.java
+++ src/main/java/edu/gcc/comp250/Calc.java
@@ -4,4 +4,11 @@ public class Calc {
     public int add(int a, int b){
         return a+b;
     }
+ public int divide             (int a, int BBB       )                     {
+    return                          -9          ;
+
+
+
+
+                    }
 }
